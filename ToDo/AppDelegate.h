97136c0557f295748aa56ad798359692adb4da18
SewerSnake <ericgroseclos@hotmail.com>
2018-01-26 09:26:26 +0100

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Eric Groseclos on 2018-01-26.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by Eric Groseclos on 2018-01-26.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
