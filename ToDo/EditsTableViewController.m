2505b78cbd755f8c1c3b9975a6607b5a84aac69d
SewerSnake <ericgroseclos@hotmail.com>
2018-01-26 18:50:31 +0100

    Changes between two images when clicking the star, to represent an important task.

--- ToDo/EditsTableViewController.m
+++ ToDo/EditsTableViewController.m
@@ -10,96 +10,63 @@
 
 @interface EditsTableViewController ()
 
+@property (nonatomic) UIButton *flagButton;
+
+@property (nonatomic) BOOL isImportant;
+
 @end
 
 @implementation EditsTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIButton *flagButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.isImportant = NO;
+    [self createButton];
+
+}
+
+// Creates a button that allows the user to
+// mark the task as extra important.
+- (void)createButton {
+    self.flagButton = [UIButton buttonWithType:UIButtonTypeCustom];
     
-    [flagButton addTarget:self action:@selector(flagMethod) forControlEvents:UIControlEventTouchUpInside];
+    [self.flagButton addTarget:self action:@selector(flagMethod) forControlEvents:UIControlEventTouchUpInside];
     
-    [flagButton setTitle:@"Flag" forState:UIControlStateNormal];
+    [self.flagButton setTitle:@"" forState:UIControlStateNormal];
     
-    flagButton.frame = CGRectMake(300, 14, 60 , 60);
+    self.flagButton.frame = CGRectMake(300, 8, 30 , 30);
     
-    [flagButton setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
+    [self.flagButton setBackgroundImage: [UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
     
-    [self.view addSubview:flagButton];
+    [self.view addSubview:self.flagButton];
+}
+
+// Shows if the task is important or not.
+// Represented by two images.
+- (void)flagMethod {
+    if (self.isImportant) {
+        [self.flagButton setBackgroundImage: [UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
+        self.isImportant = NO;
+        NSLog(@"Not important!");
+    } else {
+        [self.flagButton setBackgroundImage: [UIImage imageNamed:@"star2.png"] forState:UIControlStateNormal];
+        self.isImportant = YES;
+        NSLog(@"Important Task!");
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
--(void)flagMethod {
-    NSLog(@"Button Test");
-}
 
-/*#pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
 }
-*/
+
 
 @end
